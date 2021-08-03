ca8034ac4014cd9db4efa83904bbb79a3e61f4ba
alex doo <taejoonn@yahoo.com>
2021-08-03 14:54:28 -0400

    Added config.go

--- qt/wallet.go
+++ qt/wallet.go
@@ -24,12 +24,14 @@ func showWallet() {
 
 	// for an option to add balances of different tokens, create group
 	balanceLabel := widgets.NewQLabel2("Balance: ", nil, 0)
-	// addr := ctx.Value(ZapCommon.PublicAddress).(common.Address)
-	// instance := ctx.Value(ZapCommon.MasterContractContextKey).(*contracts.ZapMaster)
+	addr := ctx.Value(ZapCommon.PublicAddress).(common.Address)
+	instance := ctx.Value(ZapCommon.MasterContractContextKey).(*contracts.ZapMaster)
 	// zapBalance, err := instance.BalanceOf(nil, addr)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
+	fmt.Println(addr)
+	fmt.Println(instance)
 
 	// balanceBox := widgets.NewQLabel2(zapBalance.String(), nil, 0)
 
