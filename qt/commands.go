335cb816cbbae03c9121820eba127e51d7ec52c8
alex doo <taejoonn@yahoo.com>
2021-08-03 12:46:28 -0400

    Removed GPU miner

--- qt/commands.go
+++ qt/commands.go
@@ -5,26 +5,29 @@ import (
 	"crypto/ecdsa"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	ZapCommon "github.com/zapproject/pythia/common"
+	"github.com/zapproject/pythia/config"
 	"github.com/zapproject/pythia/contracts"
 	"github.com/zapproject/pythia/contracts1"
 	"github.com/zapproject/pythia/contracts2"
+	"github.com/zapproject/pythia/db"
+	"github.com/zapproject/pythia/ops"
 	"github.com/zapproject/pythia/rpc"
 	"github.com/zapproject/pythia/token"
 	"github.com/zapproject/pythia/vault"
-	"github.com/zapproject/pythia/ops"
 )
 
-
-func startMiner(){
+func startMiner() {
 
 }
-
 
 func buildCtx() error {
 	cfg = config.GetConfig()
@@ -81,84 +84,116 @@ func buildCtx() error {
 	return nil
 }
 
-
 func mineCmd() {
 	remoteDS := false
-	cmd.Action = func() {
-		//create os kill sig listener
-		c := make(chan os.Signal)
-		signal.Notify(c, os.Interrupt)
-		exitChannels := make([]*chan os.Signal, 0)
+	//create os kill sig listener
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	exitChannels := make([]*chan os.Signal, 0)
 
-		cfg := config.GetConfig()
-		var ds *ops.DataServerOps
-		if !cfg.EnablePoolWorker {
-			ErrorHandler(AddDBToCtx(*remoteDS), "\U0001F5C4 initializing database \U0001F5C4")
-			if !*remoteDS {
-				ch := make(chan os.Signal)
-				exitChannels = append(exitChannels, &ch)
+	cfg := config.GetConfig()
+	var ds *ops.DataServerOps
+	if !cfg.EnablePoolWorker {
+		ErrorHandler(AddDBToCtx(remoteDS), "\U0001F5C4 initializing database \U0001F5C4")
+		if !remoteDS {
+			ch := make(chan os.Signal)
+			exitChannels = append(exitChannels, &ch)
 
-				var err error
-				ds, err = ops.CreateDataServerOps(ctx, ch)
-				if err != nil {
-					log.Fatal(err)
-				}
-				//start and wait for it to be ready
-				ds.Start(ctx)
-				<-ds.Ready()
+			var err error
+			ds, err = ops.CreateDataServerOps(ctx, ch)
+			if err != nil {
+				log.Fatal(err)
 			}
+			//start and wait for it to be ready
+			ds.Start(ctx)
+			<-ds.Ready()
 		}
-		//start miner
-		DB := ctx.Value(ZapCommon.DataProxyKey).(db.DataServerProxy)
-		//DB := ctx.Value(ZapCommon.DBContextKey).(db.DB)
-		v, err := DB.Get(db.DisputeStatusKey)
-		if err != nil {
-			fmt.Println("ignoring --- could not get dispute status.  Check if staked")
+	}
+	//start miner
+	DB := ctx.Value(ZapCommon.DataProxyKey).(db.DataServerProxy)
+	//DB := ctx.Value(ZapCommon.DBContextKey).(db.DB)
+	v, err := DB.Get(db.DisputeStatusKey)
+	if err != nil {
+		fmt.Println("ignoring --- could not get dispute status.  Check if staked")
+	}
+	status, _ := hexutil.DecodeBig(string(v))
+	if status.Cmp(big.NewInt(1)) != 0 {
+		log.Fatalf("\U0001F6AB Miner is not able to mine with status %v. Stopping all mining immediately \U00002622", status)
+	}
+	ch := make(chan os.Signal)
+	exitChannels = append(exitChannels, &ch)
+	miner, err := ops.CreateMiningManager(ctx, ch, ops.NewSubmitter())
+	if err != nil {
+		log.Fatal(err)
+	}
+	miner.Start(ctx)
+
+	//now we wait for kill sig
+	<-c
+	//and then notify exit channels
+	for _, ch := range exitChannels {
+		*ch <- os.Interrupt
+	}
+	cnt := 0
+	start := time.Now()
+	for {
+		cnt++
+		dsStopped := false
+		minerStopped := false
+
+		if ds != nil {
+			dsStopped = !ds.Running
+		} else {
+			dsStopped = true
 		}
-		status, _ := hexutil.DecodeBig(string(v))
-		if status.Cmp(big.NewInt(1)) != 0 {
-			log.Fatalf("\U0001F6AB Miner is not able to mine with status %v. Stopping all mining immediately \U00002622", status)
+
+		if miner != nil {
+			minerStopped = !miner.Running
+		} else {
+			minerStopped = true
 		}
-		ch := make(chan os.Signal)
-		exitChannels = append(exitChannels, &ch)
-		miner, err := ops.CreateMiningManager(ctx, ch, ops.NewSubmitter())
+
+		if !dsStopped && !minerStopped && cnt > 60 {
+			fmt.Printf("\U000026A0 Taking longer than expected to stop operations. Waited %v so far\n", time.Now().Sub(start))
+		} else if dsStopped && minerStopped {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	fmt.Printf("Main shutdown complete \U00002622\n")
+
+}
+
+func AddDBToCtx(remote bool) error {
+	cfg = config.GetConfig()
+	//create a db instance
+	os.RemoveAll(cfg.DBFile)
+	DB, err := db.Open(cfg.DBFile)
+	if err != nil {
+		return err
+	}
+
+	var dataProxy db.DataServerProxy
+	if remote {
+		proxy, err := db.OpenRemoteDB(DB)
 		if err != nil {
 			log.Fatal(err)
 		}
-		miner.Start(ctx)
-
-		//now we wait for kill sig
-		<-c
-		//and then notify exit channels
-		for _, ch := range exitChannels {
-			*ch <- os.Interrupt
+		dataProxy = proxy
+	} else {
+		proxy, err := db.OpenLocalProxy(DB)
+		if err != nil {
+			log.Fatal(err)
 		}
-		cnt := 0
-		start := time.Now()
-		for {
-			cnt++
-			dsStopped := false
-			minerStopped := false
+		dataProxy = proxy
+	}
+	ctx = context.WithValue(ctx, ZapCommon.DataProxyKey, dataProxy)
+	ctx = context.WithValue(ctx, ZapCommon.DBContextKey, DB)
+	return nil
+}
 
-			if ds != nil {
-				dsStopped = !ds.Running
-			} else {
-				dsStopped = true
-			}
-
-			if miner != nil {
-				minerStopped = !miner.Running
-			} else {
-				minerStopped = true
-			}
-
-			if !dsStopped && !minerStopped && cnt > 60 {
-				fmt.Printf("\U000026A0 Taking longer than expected to stop operations. Waited %v so far\n", time.Now().Sub(start))
-			} else if dsStopped && minerStopped {
-				break
-			}
-			time.Sleep(500 * time.Millisecond)
-		}
-		fmt.Printf("Main shutdown complete \U00002622\n")
+func ErrorHandler(err error, operation string) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s failed: %s\n", operation, err.Error())
 	}
 }
