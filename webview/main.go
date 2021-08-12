4f7f296fd24bde2edf6fcbb8d85b9a7c81259614
Jonathan Eid <sevenmelee@gmail.com>
2021-08-12 02:27:51 -0400

    Added bootstrap

--- webview/main.go
+++ webview/main.go
@@ -4,8 +4,42 @@ import(
 	"github.com/webview/webview"
 	// "io/ioutil"
 	"path/filepath"
+	"io"
+	// "log"
+	"net/rpc/jsonrpc"
 	"os"
+	"fmt"
 )
+
+// type Counter struct {
+// 	Value int `json:"value"`
+// }
+
+// // Add increases the value of a counter by n
+// func (c *Counter) Add(n int) int {
+// 	c.Value = c.Value + int(n)
+// 	fmt.Println(c.Value)
+// 	return c.Value
+// }
+
+// // Reset sets the value of a counter back to zero
+// func (c *Counter) Reset() {
+// 	c.Value = 0
+// }
+
+type ReadWriteCloser struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+func (rw *ReadWriteCloser) Close() error {
+	rw.ReadCloser.Close()
+	rw.WriteCloser.Close()
+	return nil
+}
+
+// Defined for the RPC package
+type Adder int
 
 func main(){
 	debug := true
@@ -22,7 +56,22 @@ func main(){
 	}
 	p := filepath.Join(filepath.Dir(ex),"public/index.html")
 
+	adder := Adder(1)
+	var ret int
+
+	rwc := new(ReadWriteCloser)
+
+	rwc.ReadCloser = os.Stdin
+	rwc.WriteCloser = os.Stdout
+
+	client := jsonrpc.NewClient(rwc)
+
+	w.Bind("add", func(a, b int) int {
+		fmt.Println("Adding",a,b)
+		client.Call("Adder.Add", &adder, &ret)
+		return a + b
+	})
+
 	w.Navigate(p)
 	w.Run()
 }
-
