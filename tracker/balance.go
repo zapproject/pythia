8a78a6f6c6d929c2aea9668dc18f06c3df32eff7
Alex D <taejoonn@gmail.com>
2022-01-18 12:47:31 -0500

    Added vault balance to tracker

--- tracker/balance.go
+++ tracker/balance.go
@@ -11,6 +11,7 @@ import (
 	"github.com/zapproject/pythia/config"
 	"github.com/zapproject/pythia/db"
 	"github.com/zapproject/pythia/rpc"
+	"github.com/zapproject/pythia/vault"
 )
 
 //BalanceTracker concrete tracker type
@@ -29,6 +30,11 @@ func (b *BalanceTracker) Exec(ctx context.Context) error {
 	//cast client using type assertion since context holds generic interface{}
 	client := ctx.Value(zapCommon.ClientContextKey).(rpc.ETHClient)
 	DB := ctx.Value(zapCommon.DBContextKey).(db.DB)
+	vaultAddress := ctx.Value((zapCommon.VaultAddress)).(common.Address)
+	vault, err := vault.NewVaultCaller(vaultAddress, client)
+	if err != nil {
+		return fmt.Errorf("error with initialising vault instance to get vault balance: %+v", err)
+	}
 
 	//get the single config instance
 	cfg := config.GetConfig()
@@ -50,13 +56,21 @@ func (b *BalanceTracker) Exec(ctx context.Context) error {
 		return err
 	}
 
+	// Gets the vault balance
+	vaultBalance, err := vault.UserBalance(nil, fromAddress)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
 	// Encodes a BigInt balance to a hex string
 	enc := hexutil.EncodeBig(balance)
 
 	// log = Prints the date/time
 	// Printf = Prints formatted data
 	// %v = Used with Printf and gets the value of a variable
-	log.Printf("Balance: %v", balance)
+	log.Printf("Wallet Balance: %v", balance)
+	log.Printf("Vault Balance: %v", vaultBalance)
 
 	// BalanceKey is the key to store/lookup account balance
 	// Stores the converted hex string balance as a slice of bytes
