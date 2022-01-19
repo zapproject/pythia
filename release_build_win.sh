83c325421921a0c80b23229baf99ca5453b10508
AlexD <taejoonn@gmail.com>
2022-01-19 17:01:44 -0500

    Fixed release_build_win & added example config.json

--- release_build_win.sh
+++ release_build_win.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-LDFLAGS="-X main.GitHash=$(git rev-parse --short HEAD)"
-# LDFLAGS="-X main.GitHash=$(git rev-parse --short HEAD) -s -w"
+# LDFLAGS="-X main.GitHash=$(git rev-parse --short HEAD)"
+LDFLAGS="-X main.GitHash=$(git rev-parse --short HEAD) -s -w"
 
 windows
-CC="x86_64-w64-mingw32-gcc-win32" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags "$LDFLAGS"
+CC="x86_64-w64-mingw32-gcc" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags "$LDFLAGS"
 
 
 
