49fd93a3da1569f3921ee9019c907f84ac88317b
AlexD <taejoonn@gmail.com>
2022-01-20 17:24:35 -0500

    Verified compatibility with Windows only as single process

--- main.go
+++ main.go
@@ -100,6 +100,7 @@ func AddDBToCtx(remote bool) error {
 	os.RemoveAll(cfg.DBFile)
 	DB, err := db.Open(cfg.DBFile)
 	if err != nil {
+		fmt.Println("Failing DB open")
 		return err
 	}
 
@@ -107,12 +108,14 @@ func AddDBToCtx(remote bool) error {
 	if remote {
 		proxy, err := db.OpenRemoteDB(DB)
 		if err != nil {
+			fmt.Println("Failing Remote DB open")
 			log.Fatal(err)
 		}
 		dataProxy = proxy
 	} else {
 		proxy, err := db.OpenLocalProxy(DB)
 		if err != nil {
+			fmt.Println("Failing local proxy open")
 			log.Fatal(err)
 		}
 		dataProxy = proxy
