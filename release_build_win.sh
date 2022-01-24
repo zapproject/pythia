0fbc0709cf49a4240c0ab69452e8c39de4beb49e
AlexD <taejoonn@gmail.com>
2022-01-24 12:18:07 -0500

    Fixed release build windows

--- release_build_win.sh
+++ release_build_win.sh
@@ -2,7 +2,7 @@
 # LDFLAGS="-X main.GitHash=$(git rev-parse --short HEAD)"
 LDFLAGS="-X main.GitHash=$(git rev-parse --short HEAD) -s -w"
 
-windows
+# windows
 CC="x86_64-w64-mingw32-gcc" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags "$LDFLAGS"
 
 
